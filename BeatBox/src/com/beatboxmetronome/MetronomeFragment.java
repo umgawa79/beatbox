5984cd82d61756f494f63acd62e22ccd29759803
jysong14 <song14@illinois.edu>
2014-10-31 06:50:15 -0500

    Added initial tempo timeline

--- BeatBox/src/com/beatboxmetronome/MetronomeFragment.java
+++ BeatBox/src/com/beatboxmetronome/MetronomeFragment.java
@@ -1,5 +1,9 @@
 package com.beatboxmetronome;
 
+import java.util.Random;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +13,8 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -109,6 +115,7 @@ public class MetronomeFragment extends Fragment implements OnClickListener
     		{
 	    		case BASIC:
 	    		{
+	    			drawTempo(v);
 	    			modeButton.setText(R.string.basic_mode_label);
 	    			inc.setVisibility(View.INVISIBLE);
 	    			dec.setVisibility(View.INVISIBLE);
@@ -118,6 +125,7 @@ public class MetronomeFragment extends Fragment implements OnClickListener
 	    		}
 	    		case TEMPLATE:
 	    		{
+	    			resetTempo(v);
 	    			modeButton.setText(R.string.template_mode_label);
 	    			inc.setVisibility(View.VISIBLE);
 	    			dec.setVisibility(View.VISIBLE);
@@ -191,6 +199,39 @@ public class MetronomeFragment extends Fragment implements OnClickListener
     			bpmText.setText(text);
     		}
     	}
+    }
+    
+    /**
+     * Draws the tempo timeline
+     * @param View v
+     */
+    private void drawTempo(View v)
+    {
+    	LinearLayout tempoTimeline = (LinearLayout) v.findViewById(R.id.tempo_container);
+        Bitmap bmap = Bitmap.createBitmap(500, 70, Bitmap.Config.ARGB_8888); 
+        Canvas canv = new Canvas(bmap);
+        ImageView tempo = new ImageView(getActivity());
+        Random rand = new Random();       
+   
+        for(int x=0;x<5;x++) {
+            Paint color = new Paint();
+            color.setARGB(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+            canv.drawRect(x*100, 70, (x*100)+100, 0, color);                  
+        }               
+        
+        tempo.setImageBitmap(bmap);
+        tempo.setScaleType(ImageView.ScaleType.FIT_START);
+        tempoTimeline.addView(tempo);
+    }
+    
+    /**
+     * Resets the tempo timeline
+     * @param View v
+     */
+    private void resetTempo(View v)
+    {
+    	LinearLayout tempoTimeline = (LinearLayout) v.findViewById(R.id.tempo_container);
+    	tempoTimeline.removeAllViews();
     }
 
 }
