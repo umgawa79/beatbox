de80bc9eb681ebfc309d27a696e25c626b3f509d
jstrafford4 <jstrafford4@gmail.com>
2014-11-10 18:30:31 -0600

    Edit Tab is close but not complete

--- BeatBox/src/com/beatboxmetronome/EditFragment.java
+++ BeatBox/src/com/beatboxmetronome/EditFragment.java
@@ -39,6 +39,7 @@ public class EditFragment extends Fragment
     private TempoSectionsAdapter tsAdapter;
     private Context c;
     private boolean templateView = true;
+    private int pos = -1; //I'm using this to carry position variable for onclick events for section rows, while having the buttons defined outside that onclick
     
     View curView;
 	
@@ -53,6 +54,8 @@ public class EditFragment extends Fragment
 		Log.d("BeatBox", "onCreate starting");
 		c = getActivity();
 		currentDir = c.getFilesDir(); //TODO: fix pathing.
+		File localDir = new File(currentDir + "/local/");
+		currentDir = localDir;
 		System.out.println(currentDir.getAbsolutePath());
 		fill(currentDir);
 		Log.d("BeatBox", "onCreate finishing");
@@ -64,7 +67,7 @@ public class EditFragment extends Fragment
 				{
 					if (templateView)
 					{
-						System.out.println("Item selected at position: "+position);
+						System.out.println("Inside ItemClick for list of templates at position: "+position);
 						Template t = adapter.getItem(position);
 						onTemplateSelected(t);
 						templateView = false;
@@ -75,9 +78,18 @@ public class EditFragment extends Fragment
 					}
 				} 
 		});
+		
+        Button createTemplateButton = (Button) fragView.findViewById(R.id.createNewTemplateButton);
+        createTemplateButton.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		onTemplateSelected(new Template());
+        	}
+        });
 		return fragView;
 	
 	}
+	
+	
 	
 	private void fill(File f)
 	{
@@ -133,7 +145,7 @@ public class EditFragment extends Fragment
 	
 	
 	
-	public void onTemplateSelected(Template t)
+	public void onTemplateSelected(final Template t)
 	{	// May need to delete this if I use mCallback
 		System.out.println("Here's where I'd send the template to other fragments.");
 		final List<Vector> sections = new ArrayList<Vector>();
@@ -151,12 +163,16 @@ public class EditFragment extends Fragment
 		
 		ViewGroup parent = (ViewGroup) curView.getParent();
 	    int index = parent.indexOfChild(curView);
+	    final View templatesView = curView;
 	    parent.removeView(curView);
 	    LayoutInflater inflater = LayoutInflater.from(c);
 	    curView = inflater.inflate(R.layout.fragment_create_layout, parent, false);
 	    parent.addView(curView, index);
 	    
         listView = (ListView) curView.findViewById(R.id.sectionList);
+        
+		final LinearLayout editSpecificSection = (LinearLayout) curView.findViewById(R.id.editSpecificSection);
+		final LinearLayout playSaveBar = (LinearLayout) curView.findViewById(R.id.playSaveBar);
     
         
 		listView.setOnItemClickListener(new OnItemClickListener() {
@@ -165,75 +181,14 @@ public class EditFragment extends Fragment
 					{
 						
 						System.out.println("Item clicked in edit mode at position: " + position + ". Can you believe it?!");
-						final LinearLayout editSpecificSection = (LinearLayout) curView.findViewById(R.id.editSpecificSection);
-						final LinearLayout playSaveBar = (LinearLayout) curView.findViewById(R.id.playSaveBar);
 						playSaveBar.setVisibility(View.GONE);
+						pos = position;
 						editSpecificSection.setVisibility(View.VISIBLE);
-						
-						
-						
-						Button okButton = (Button) editSpecificSection.findViewById(R.id.sectionEditOK);
-						okButton.setOnClickListener(new View.OnClickListener() {
-							public void onClick(View v) {
-								EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
-								int newBPM = Integer.parseInt(bpmEditText.getText().toString());
-								EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
-								int newMeasures = Integer.parseInt(measuresEditText.getText().toString());
-								
-								if((newBPM > 0) && (newMeasures > 0))
-								{
-									tempoVector.set(position, newBPM);
-									measuresVector.set(position, newMeasures);
-									
-									Vector<Integer> newSection = (Vector<Integer>) sections.get(position);
-									newSection.set(0, newBPM);
-									newSection.set(1, newMeasures);
-									
-									sections.set(position, newSection);
-									
-									bpmEditText.setText("");
-									measuresEditText.setText("");
-									editSpecificSection.setVisibility(View.GONE);
-									playSaveBar.setVisibility(View.VISIBLE);
-									
-									tsAdapter.notifyDataSetChanged();
-					
-								}
-							}
-	
-						});
-						
-						final Button cancelButton = (Button) editSpecificSection.findViewById(R.id.sectionEditCancel);
-			            cancelButton.setOnClickListener(new View.OnClickListener() {
-			                public void onClick(View v) {
-								EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
-								EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
-								
-								bpmEditText.setText("");
-								measuresEditText.setText("");
-			                    editSpecificSection.setVisibility(View.GONE);
-			                    playSaveBar.setVisibility(View.VISIBLE);
-			                }
-			            });
-			            
-			            final Button deleteButton = (Button) editSpecificSection.findViewById(R.id.sectionEditDelete);
-			            deleteButton.setOnClickListener(new View.OnClickListener() {
-			            	public void onClick(View v) {
-			            		sections.remove(position);
-			            		tsAdapter.notifyDataSetChanged();
-			            		
-								EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
-								EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
-								bpmEditText.setText("");
-								measuresEditText.setText("");
-			                    editSpecificSection.setVisibility(View.GONE);
-			                    playSaveBar.setVisibility(View.VISIBLE);
-			            	}
-			            });
 						
 						
 					} 
 			});
+		
 	    
 	    
 		tsAdapter = new TempoSectionsAdapter(c,R.layout.temposectionbar,sections);
@@ -241,6 +196,154 @@ public class EditFragment extends Fragment
 		//maybe invalidate and redraw here, replace view
 		
 		tsAdapter.notifyDataSetChanged();
+		
+		Button saveButton = (Button) curView.findViewById(R.id.saveCreateButton);
+		saveButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				t.setTemposVector(tempoVector);
+				t.setMeasuresVector(measuresVector);
+				
+				ViewGroup parent = (ViewGroup) curView.getParent();
+			    int index = parent.indexOfChild(curView);
+			    parent.removeView(curView);
+			    LayoutInflater inflater = LayoutInflater.from(c);
+			    curView = inflater.inflate(R.layout.fragment_save, parent, false);
+			    
+				final EditText saveTitle = (EditText) curView.findViewById(R.id.save_edit_title);
+				saveTitle.setText(t.getTemplateName());
+				
+				final EditText saveComposer = (EditText) curView.findViewById(R.id.save_edit_composer);
+				saveComposer.setText(t.getComposer());
+				
+				final EditText saveDescription = (EditText) curView.findViewById(R.id.save_edit_description);
+				saveDescription.setText(t.getDescription());
+				
+				final EditText saveCreator = (EditText) curView.findViewById(R.id.save_edit_creator);
+				saveCreator.setText(t.getCreator());
+				
+				parent.addView(curView, index);
+				
+				Button saveInfoButton = (Button) curView.findViewById(R.id.save_button_save);
+				saveInfoButton.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						t.setTemplateName(saveTitle.getText().toString());
+						t.setComposer(saveComposer.getText().toString());
+						t.setDescription(saveDescription.getText().toString());
+						t.setCreator(saveCreator.getText().toString());
+						
+						try {
+							t.saveTemplate();
+						} catch(IOException e) {
+							System.out.println("Exception trying to save template");
+						}
+						
+						ViewGroup parent = (ViewGroup) curView.getParent();
+					    int index = parent.indexOfChild(curView);
+					    parent.removeView(curView);
+					    LayoutInflater inflater = LayoutInflater.from(c);
+					    curView = templatesView;
+					    parent.addView(curView, index);
+						
+						
+						
+					}
+				});
+				
+				
+		        
+				
+			}
+		});
+		
+		Button newSectionButton = (Button) curView.findViewById(R.id.createNewSectionButton);
+		newSectionButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				playSaveBar.setVisibility(View.GONE);
+				pos = -1;
+				editSpecificSection.setVisibility(View.VISIBLE);
+			}
+		});
+		
+				
+		Button okButton = (Button) editSpecificSection.findViewById(R.id.sectionEditOK);
+		okButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
+					int newBPM = Integer.parseInt(bpmEditText.getText().toString());
+					EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
+					int newMeasures = Integer.parseInt(measuresEditText.getText().toString());
+
+						
+
+					if((newBPM > 0) && (newMeasures > 0))
+					{
+						if(pos < 0)
+						{	
+							tempoVector.add(newBPM);
+							measuresVector.add(newMeasures);
+								
+							Vector<Integer> newSection = new Vector<Integer>();
+							newSection.add(newBPM);
+							newSection.add(newMeasures);
+							newSection.add(4);
+								
+							sections.add(newSection);	
+						}
+						else
+						{
+							tempoVector.set(pos,  newBPM);
+							measuresVector.set(pos, newMeasures);
+							
+							Vector<Integer> newSection = sections.get(pos);
+							newSection.set(0, newBPM);
+							newSection.set(1, newMeasures);
+							
+							sections.set(pos, newSection);
+						}
+							
+							bpmEditText.setText("");
+							measuresEditText.setText("");
+							editSpecificSection.setVisibility(View.GONE);
+							playSaveBar.setVisibility(View.VISIBLE);
+							
+							tsAdapter.notifyDataSetChanged();
+					}
+			}
+		});
+				
+		
+		final Button cancelButton = (Button) editSpecificSection.findViewById(R.id.sectionEditCancel);
+	    cancelButton.setOnClickListener(new View.OnClickListener() {
+	    	public void onClick(View v) {
+					EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
+					EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
+						
+					bpmEditText.setText("");
+					measuresEditText.setText("");
+	                editSpecificSection.setVisibility(View.GONE);
+	                playSaveBar.setVisibility(View.VISIBLE);
+	                }
+	            });
+	            
+	    final Button deleteButton = (Button) editSpecificSection.findViewById(R.id.sectionEditDelete);
+	    deleteButton.setOnClickListener(new View.OnClickListener() {
+	    	public void onClick(View v) {
+	    			if(pos >= 0)
+	    			{
+	    				
+	    				sections.remove(pos);
+	    				tsAdapter.notifyDataSetChanged();
+	            		
+	    				EditText bpmEditText = (EditText) editSpecificSection.findViewById(R.id.editBPM);
+	    				EditText measuresEditText = (EditText) editSpecificSection.findViewById(R.id.editMeasures);
+						bpmEditText.setText("");
+						measuresEditText.setText("");
+						editSpecificSection.setVisibility(View.GONE);
+						playSaveBar.setVisibility(View.VISIBLE);
+	            	}
+
+			}
+		});
 		
 		
 	}
